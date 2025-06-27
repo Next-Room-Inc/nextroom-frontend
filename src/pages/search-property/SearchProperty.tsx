70dbcc67bc2fb1966fa83ce01dc951c7b75c3c88
Paul Brooks <paul.brooks@nextroom.ca>
2025-06-27 11:55:44 -0400

    added requested tour modal

--- src/pages/search-property/SearchProperty.tsx
+++ src/pages/search-property/SearchProperty.tsx
@@ -1,7 +1,7 @@
 import { ArrowLeftIcon, ArrowRightIcon, ChevronUpIcon } from '@heroicons/react/16/solid'
-import { ChevronDownIcon, XMarkIcon } from '@heroicons/react/20/solid'
+import { CheckCircleIcon, ChevronDownIcon, XMarkIcon } from '@heroicons/react/20/solid'
 import { AnimatePresence, motion } from "framer-motion"
-import { useState } from 'react'
+import { Fragment, useState } from 'react'
 import SearchPropertyLayout from '../../layouts/SearchProperty.Layout'
 import { HousingCard, PrimaryButton } from './ComponComponents'
 
@@ -85,12 +85,192 @@ const SearchProperty = () => {
                 {/* Chat Sticky Button */}
                 <ChatButton />
                 {/* AvailableUnitsModal */}
+                <div className='mx-10 md:mx-20'>
+                    <h2 className="text-lg font-semibold text-center md:text-left text-gray-700 mb-4">Requested Tour(s)</h2>
+                    <TourTimeline />
+                </div>
             </SearchPropertyLayout>
         </>
     )
 }
 
-export default SearchProperty
+export default SearchProperty;
+
+
+const TourTimeline = () => {
+    const tourData = [
+        {
+            name: "You",
+            status: "Requested",
+            date: "September 15 2025",
+            image: "/assets/img/search-property/student_profile (1).png", // replace with your image path
+            color: "text-gray-600",
+        },
+        {
+            name: "uOttawa Student",
+            status: "Accepted",
+            date: "September 15 2025",
+            image: "/assets/img/search-property/student_profile (2).png",
+            color: "text-green-600",
+        },
+        {
+            name: "uOttawa Student",
+            status: "Pending",
+            date: "September 15 2025",
+            image: "/assets/img/search-property/student_profile (3).png",
+            color: "text-yellow-600",
+        },
+    ];
+
+    return (
+        <div className="bg-white rounded-4xl shadow-md p-4 w-full  px-8 mx-auto">
+
+
+            <div className={`flex  flex-col md:items-center md:flex-row justify-between py-10`}>
+                {tourData.map((item, index) => (
+                    <Fragment key={index}>
+
+                        {index !== 0 && (
+                            <>
+                                <div className={`hidden md:block h-1  border-t-4 border-[#B3322F] mx-2 ${[0, 1].includes(index) ? 'w-full' : 'w-80'}`}> </div>
+                                <div className={`block md:hidden w-1  ml-15 border-l-4 border-[#B3322F] my-2 ${[0, 1].includes(index) ? 'h-30' : 'h-15'}`}></div>
+                            </>
+                        )}
+
+
+
+                        <div className="flex md:flex-col items-center md:text-center min-w-35 md:-mx-4.5  -my-3 overflow-hidden">
+                            <p className="hidden md:block mt-2 text-sm font-semibold   w-full mb-2">{item.name}</p>
+                            <div className="w-30 h-30 rounded-full overflow-hidden border-2 border-gray-300">
+                                <img src={item.image} alt={item.name} className='w-full h-full' />
+                            </div>
+                            <div className='md:ml-0 ml-4'>
+                                <p className="md:hidden block mt-2 text-sm font-semibold   w-full">{item.name}</p>
+
+                                <p className={`text-md font-semibold ${item.color}`}>{item.status}</p>
+                                <p className="text-sm  w-full">{item.date}</p>
+                            </div>
+                        </div>
+
+
+                        {index === 0 && (
+                            <>
+
+
+                                <div className="hidden md:block h-1 w-full border-t-4 border-[#B3322F] mx-2"></div>
+                                <div className="block md:hidden w-1 ml-15 h-30 border-l-4 border-[#B3322F] my-2"></div>
+
+
+                                <div className={`flex md:flex-col items-center  md:-mx-4.5 -my-4.5  `}>
+                                    <p className="hidden md:block text-md font-semibold   -mt-10 md:ml-0 ml-4">Let's Go!</p>
+                                    <CheckCircleIcon className="w-30 h-30 md:w-20 md:h-20 text-[#B3322F]" />
+                                    <p className="md:hidden block text-md font-semibold    md:ml-0 ml-4">Let's Go!</p>
+
+                                </div>
+                            </>
+                        )}
+
+
+                    </Fragment>
+                ))}
+            </div>
+
+            <div className="my-4 border-t pt-4 md:px-4 text-sm text-gray-800 text-center md:flex gap-5 items-center">
+                <p className='md:mt-2'><span className="font-semibold">Unit 308</span></p>
+                <button className="mt-2 px-4 py-1 text-sm border border-[#B3322F] rounded-full text-[#B3322F] hover:bg-red-50 transition">
+                    Withdraw Tour Request
+                </button>
+            </div>
+        </div>
+    );
+}
+// const TourTimeline = () => {
+//     const isMobile = true
+//     const tourData = [
+//         {
+//             name: "You",
+//             status: "Requested",
+//             date: "September 15 2025",
+//             image: "/assets/img/search-property/student_profile (1).png", // replace with your image path
+//             color: "text-gray-600",
+//         },
+//         {
+//             name: "uOttawa Student",
+//             status: "Accepted",
+//             date: "September 15 2025",
+//             image: "/assets/img/search-property/student_profile (2).png",
+//             color: "text-green-600",
+//         },
+//         {
+//             name: "uOttawa Student",
+//             status: "Pending",
+//             date: "September 15 2025",
+//             image: "/assets/img/search-property/student_profile (3).png",
+//             color: "text-yellow-600",
+//         },
+//     ];
+
+//     return (
+//         <div className="bg-white rounded-xl shadow-md p-4 w-full max-w-4xl mx-auto">
+//             <h2 className="text-md font-medium text-gray-700 mb-4">Requested Tour(s)</h2>
+
+//             <div className={`flex ${isMobile ? "flex-col items-center" : "flex-row justify-between items-center"} bg-red-100`}>
+//                 {tourData.map((item, index) => (
+//                     <Fragment key={index}>
+
+//                       {index !== 0 && !isMobile && (
+//                             <div className="h-1 w-full border-t-2 border-[#B3322F] mx-2 bg-green-100">asd</div>
+//                         )}
+//                         {index !== 0 && isMobile && (
+//                             <div className="w-1 h-8 border-l-2 border-red-500 my-2"></div>
+//                         )}
+
+
+//                         <div className="flex flex-col items-center text-center">
+//                             <div className="w-16 h-16 rounded-full overflow-hidden border-2 border-gray-300">
+//                                 <img src={item.image} alt={item.name} width={64} height={64} />
+//                             </div>
+//                             <p className="mt-2 text-sm font-semibold">{item.name}</p>
+//                             <p className={`text-xs ${item.color}`}>{item.status}</p>
+//                             <p className="text-xs text-gray-500">{item.date}</p>
+//                         </div>
+
+
+//                         {index === 0 && (
+//                             <>
+
+//                       { !isMobile && (
+//                             <div className="h-1 w-full border-t-2 border-red-500 mx-2"></div>
+//                         )}
+//                         { isMobile && (
+//                             <div className="w-1 h-8 border-l-2 border-red-500 my-2"></div>
+//                         )}
+
+//                             <div className={`flex flex-col items-center ${isMobile ? "my-2" : "mx-4"}`}>
+//                                 <div className="w-10 h-10 rounded-full bg-gray-100 flex items-center justify-center border-2 border-gray-300">
+//                                     <CheckCircleIcon className="w-5 h-5 text-gray-400" />
+//                                 </div>
+//                                 {!isMobile && (
+//                                     <p className="text-xs text-gray-500 mt-1">Let's Go!</p>
+//                                 )}
+//                             </div>
+//                             </>
+//                         )}
+
+
+//                     </Fragment>
+//                 ))}
+//             </div>
+
+//             <div className="my-4 border-t pt-3 text-sm text-gray-800 text-center">
+//                 <p><span className="font-semibold">Unit 308</span></p>
+//                 <button className="mt-2 px-4 py-1 text-sm border border-[#B3322F] rounded-full text-[#B3322F] hover:bg-red-50 transition">
+//                     Withdraw Tour Request
+//                 </button>
+//             </div>
+//         </div>
+//     );
+// }
 
 
 const ChatButton = () => {
@@ -500,11 +680,11 @@ const ResponsiveTabSelector = () => {
 // 🔽 Dropdown Component
 type NumberInputProps = {
     label: string;
-    value: number  ;
+    value: number;
     onChange: (val: number) => void;
 };
 
-function NumberInput({ label, value = 0 , onChange }: NumberInputProps) {
+function NumberInput({ label, value = 0, onChange }: NumberInputProps) {
     const handleIncrement = () => onChange(value + 1);
     const handleDecrement = () => onChange(Math.max(0, value - 1));
 
