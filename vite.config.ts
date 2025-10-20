b363afd6a52896122b41da13b359e5f2d06a93a1
NextRoomInc <info@nextroom.ca>
2025-10-20 18:24:15 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,11 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
-import { fileURLToPath } from "url";
-import { dirname, resolve } from "path";
-
-// Fix __dirname for ES modules
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-// https://vite.dev/config/
 export default defineConfig({
-  server: {
-    port: 5000,
-  },
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@src": resolve(__dirname, "src/"),
     },
   },
-  plugins: [react(), tailwindcss()],
+  build: {
+    outDir: "dist" // <-- make sure this is set
+  }
 });
