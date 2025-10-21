a720c724134db49026f64f1af748f136a6be929e
NextRoomInc <info@nextroom.ca>
2025-10-20 21:40:38 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  server:{
+  server: {
     port: 5000,
   },
   plugins: [react(), tailwindcss()],
